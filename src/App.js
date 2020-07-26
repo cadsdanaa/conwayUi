e52043ba376a24bdae253566b965c447d7ee1419
Alex Dana <cadsdanaa@gmail.com>
2020-07-26 15:35:22 -0500

    Initial skeleton commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Conway's Game of Life
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/cadsdanaa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
